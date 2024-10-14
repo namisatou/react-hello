8479c85a804197572d499ca3de9e4360e9b2203a
namisatou <damianrod007@gmail.com>
2024-10-14 23:07:46 +0000

    First commit

---
+++ src/js/component/Card.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+const Card = () => {
+
+    return (
+        <div className="col-xs-12 col-sm-12 col-md-6 col-lg-3 mb-4">
+            <div className="card">
+                <img src="https://picsum.photos/200" className="card-img-top" alt="..." />
+                <div className="card-body">
+                    <h5 className="card-title">Card title - Yojan</h5>
+                    <p className="card-text">
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur dolor excepturi nam officiis distinctio, sapiente impedit itaque qui fuga repellat sint dignissimos nesciunt, ipsam provident alias incidunt! Deleniti, expedita earum?
+                    </p>
+                    <a href="#" className="btn btn-primary">Find Out More!</a>
+                </div>
+            </div>
+        </div>
+    );
+    
+};
+
+export default Card;
--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,4 +1,7 @@
 import React from "react";
+import Card from "./Card";
+import Footer from "./Footer";
+import Jumbotron from "./Jumbotron";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
@@ -6,21 +9,21 @@ import rigoImage from "../../img/rigo-baby.jpg";
 //create your first component
 const Home = () => {
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="text-center container">
+			<h1 className="text-center mt-5">Landing Page React</h1>
+			<div>
+            	<Jumbotron />
+        	</div>
+			<div className="row">
+                <Card />
+                <Card />
+                <Card />
+                <Card />
+            </div>
+			<Footer />
 		</div>
 	);
 };
 
 export default Home;
+
